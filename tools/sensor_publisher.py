411a6c296d49b5f95d9e7b487b26fd6007261864
Angelo Pettinelli <angelo.pettinelli@gashapp.com>
2021-05-03 21:24:03 +0200

    tools files

---
+++ tools/sensor_publisher.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import json
+
+from random import random
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+def on_publish(client, userdata, result):
+    print(f"data published \nresult {result}")
+
+
+def random_sensors():
+    data = {}
+    data['temperature'] = random()*100
+    data['pressure'] = random()*100
+    data['voltage'] = random()*10
+
+    return data
+
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+
+    client.connect("localhost", 1883, 60)
+
+    try:
+        while True:
+            input("Press Enter to continue...")
+            client.publish("sensors/", json.dumps(random_sensors()))
+
+    except KeyboardInterrupt:
+        pass
